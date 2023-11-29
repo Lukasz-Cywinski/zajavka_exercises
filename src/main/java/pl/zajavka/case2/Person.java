7dee3e1b699ad0acb699ed41e89ac4ba75cfffe7
Lukasz Cywinski <lukasz.cywin@gmail.com>
2023-11-29 07:26:27 +0100

    Person modification

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -1,12 +1,18 @@
 package pl.zajavka.case2;
+import java.math.BigDecimal;
 
-record Person(String name, String surname) {
+record Person(String name, Long age, BigDecimal salary) {
 
-    public String whatIsYourJob() {
-        return "Software developer!";
+    public Boolean doYouHaveABike() {
+        return true;
     }
 
-    public String whatDoYouLike() {
-        return "Strawberries";
+
+    public BigDecimal howMuchDoYouEarn() {
+        return salary;
+    }
+
+    public Boolean doYouLikePizza() {
+        return true;
     }
 }
